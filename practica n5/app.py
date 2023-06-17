c8b35768bc053a3d75b4ac45dfcfd3b7c12b0501
adrianastyles <125921265+adrianastyles@users.noreply.github.com>
2023-06-16 18:47:41 -0600

    modificaciones en app.py

--- practica n5/app.py
+++ practica n5/app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, request #importación de libreria
 
-app = Flask(__name__)
+#iniciazicón del servidor Flask
 
+#Configuración para la BD
+app= Flask(__name__)
+app.config['MYSQL_HOST']= "localhost" #Aqui se especica el servicio/host/ip de un servidor
+app.config['MYSQL_USER']="root"
+app.config['MYSQL_PASSWORD']=""
+app.config['MYSQL_DB']="dbflask"
+
+#Declaración de Ruta 
+
+#Ruta index http://localhost:5000
+#La ruta se compone del nombre de la ruta y su función
 @app.route('/')
 def index():
-    return "Hola mundo"
+    return render_template('index.html')
 
-if __name__== '__main__':
-    app.run(port = 5000)
+@app.route('/guardar', methods=['POST'])
+def guardar():
+    if request.method == 'POST':
+        titulo = request.form['txtTitulo']
+        artista = request.form['txtArtista']
+        anio = request.form['txtAnio']
+        print(titulo, artista, anio)
+    
+    return 'La info del album llego a la ruta'
+
+@app.route('/eliminar')
+def eliminar():
+    return "Se elimino de la BD"
+
+# Lineas que ejecutan el servidor
+if __name__ =='__main__':
+    app.run(port= 5000, debug=True)
