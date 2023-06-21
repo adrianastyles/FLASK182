d32da38d15eabfb47cd381c01301f0698f2af27a
adrianastyles <125921265+adrianastyles@users.noreply.github.com>
2023-06-20 18:49:05 -0600

    app.py

--- practica n5/app.py
+++ practica n5/app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template, request #importación de libreria
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL #importación de libreria
 
 #iniciazicón del servidor Flask
 
 #Configuración para la BD
 app= Flask(__name__)
-app.config['MYSQL_HOST']= "localhost" #Aqui se especica el servicio/host/ip de un servidor
-app.config['MYSQL_USER']="root"
-app.config['MYSQL_PASSWORD']=""
-app.config['MYSQL_DB']="dbflask"
+app.config['MYSQL_HOST']= 'localhost' #Aqui se especica el servicio/host/ip de un servidor
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='dbflask'
+app.secret_key='mysecretkey'
+
+mysql = MySQL(app)
 
 #Declaración de Ruta 
 
@@ -23,9 +27,14 @@ def guardar():
         titulo = request.form['txtTitulo']
         artista = request.form['txtArtista']
         anio = request.form['txtAnio']
-        print(titulo, artista, anio)
+        #print(titulo, artista, anio)
+        CS = mysql.connection.cursor()
+        CS.execute('insert into albums1(titulo,artista,anio) values(%s,%s,%s)',(titulo, artista, anio))
+        mysql.connection.commit()
     
-    return 'La info del album llego a la ruta'
+    
+    flash('Album agregado correctamente')
+    return redirect(url_for('index'))
 
 @app.route('/eliminar')
 def eliminar():
