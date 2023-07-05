898d72c24bf1a8373bb974fcb8c7c004cb34fae1
adrianastyles <125921265+adrianastyles@users.noreply.github.com>
2023-07-04 18:55:19 -0600

    pract8: actualización completa

--- practica n5/app.py
+++ practica n5/app.py
@@ -38,13 +38,56 @@ def guardar():
         CS.execute('insert into albums1(titulo,artista,anio) values(%s,%s,%s)',(titulo, artista, anio))
         mysql.connection.commit()
     
-    
     flash('Album agregado correctamente')
     return redirect(url_for('index'))
 
-@app.route('/eliminar')
-def eliminar():
-    return "Se elimino de la BD"
+@app.route('/editar/<id>')
+def editar(id):
+    curEditar=mysql.connection.cursor()
+    curEditar.execute('select * from albums1 where id_album = %s ', (id,))
+    consulID = curEditar.fetchone()
+    
+    return render_template('editaralbum.html', album=consulID)
+
+
+@app.route('/actualizar/<id>', methods=['POST'])
+def actualizar(id):
+    if request.method == 'POST':
+        
+        titulo = request.form['txtTitulo']
+        artista = request.form['txtArtista']
+        anio = request.form['txtAnio']
+        
+        curAct = mysql.connection.cursor()
+        curAct.execute('update albums1 set titulo=%s,artista=%s,anio=%s where id_album=%s',(titulo, artista, anio, id))
+        mysql.connection.commit()
+        
+        flash('Album actualizado correctamente')
+        return redirect(url_for('index'))
+    
+@app.route('/eliminar/<id>')
+def eliminar(id):
+    curEliminar=mysql.connection.cursor()
+    curEliminar.execute('select * from albums1 where id_album = %s ', (id,))
+    consulID1 = curEliminar.fetchone()
+    
+    return render_template('eliminaralbum.html', album=consulID1)
+
+@app.route('/delete/<id>', methods=['POST'])
+def delete(id):
+    if request.method == 'POST':
+        
+        titulo = request.form['txtTitulo']
+        artista = request.form['txtArtista']
+        anio = request.form['txtAnio']
+        
+        curAct = mysql.connection.cursor()
+        curAct.execute('delete from albums1 where id_album=%s',(id))
+        mysql.connection.commit()
+        
+        flash('Album actualizado correctamente')
+        return redirect(url_for('index'))
+    
 
 # Lineas que ejecutan el servidor
 if __name__ =='__main__':
