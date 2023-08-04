9748ebb8de475972d24248e5e847e34806d5a6a3
adrianastyles <125921265+adrianastyles@users.noreply.github.com>
2023-08-03 18:18:45 -0600

    Bootstrap
    Se modificaron las interfaces para meter CSS (Bootstrap) en las interfaces index, editar y eliminar y se agregó una plantilla llamada "Plantillas".

--- practica n5/app.py
+++ practica n5/app.py
@@ -1,94 +1,83 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_mysqldb import MySQL #importación de libreria
+#importacion del framework
+from flask import Flask,render_template,request,redirect,url_for,flash
+from flask_mysqldb import MySQL
 
-#iniciazicón del servidor Flask
 
-#Configuración para la BD
+#inicializacion del APP o servidor
 app= Flask(__name__)
-app.config['MYSQL_HOST']= 'localhost' #Aqui se especica el servicio/host/ip de un servidor
+#Configuracion de la conexion
+app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']=''
 app.config['MYSQL_DB']='dbflask'
 app.secret_key='mysecretkey'
+mysql=MySQL(app)
 
-mysql = MySQL(app)
 
-#Declaración de Ruta 
-
-#Ruta index http://localhost:5000
-#La ruta se compone del nombre de la ruta y su función
+#declaracion de ruta  http://localhost:5000 
 @app.route('/')
-
 def index():
-    curSelect=mysql.connection.cursor()
-    curSelect.execute('select * from albums1')
-    consulta = curSelect.fetchall()
-    #print(consulta)
-    
-    return render_template('index.html', listAlbums1=consulta)
+    CC=mysql.connection.cursor()
+    CC.execute('select * from albums1')
+    conAlbums=CC.fetchall()
+    #print(conAlbums)
+    return render_template('index.html', listAlbums=conAlbums)
 
+#ruta http:localhost:5000/guaradr tipo POST para Insert
 @app.route('/guardar', methods=['POST'])
 def guardar():
-    if request.method == 'POST':
-        titulo = request.form['txtTitulo']
-        artista = request.form['txtArtista']
-        anio = request.form['txtAnio']
-        #print(titulo, artista, anio)
-        CS = mysql.connection.cursor()
-        CS.execute('insert into albums1(titulo,artista,anio) values(%s,%s,%s)',(titulo, artista, anio))
+    if request.method =='POST':
+        #pasamos a variables el contenido de los inputs
+        Vtitulo=request.form['txtTitulo']
+        Vartista=request.form['txtArtista']
+        Vanio=request.form['txtAnio']
+        #Conectar y ejecutar  el insert
+        CS=mysql.connection.cursor()
+        CS.execute('insert into albums1(titulo,artista,anio) values(%s,%s,%s)',(Vtitulo, Vartista, Vanio))
         mysql.connection.commit()
+        #print(titulo,artista,anio)
     
-    flash('Album agregado correctamente')
-    return redirect(url_for('index'))
+    flash('El album fue agregado correctamente')
+    return redirect(url_for('index'))    
 
 @app.route('/editar/<id>')
 def editar(id):
-    curEditar=mysql.connection.cursor()
-    curEditar.execute('select * from albums1 where id_album = %s ', (id,))
-    consulID = curEditar.fetchone()
-    
-    return render_template('editaralbum.html', album=consulID)
+    cursoID=mysql.connection.cursor()
+    cursoID.execute('select * from albums1 where id_album=%s',(id))
+    constID=cursoID.fetchone()
+    return render_template('editaralbum.html',album=constID)
 
-
-@app.route('/actualizar/<id>', methods=['POST'])
+@app.route('/actualizar/<id>',methods=['POST'])
 def actualizar(id):
-    if request.method == 'POST':
+    if request.method =='POST':
+        Vartitulo=request.form['txtTitulo']
+        Varartista=request.form['txtArtista']
+        Varanio=request.form['txtAnio']
         
-        titulo = request.form['txtTitulo']
-        artista = request.form['txtArtista']
-        anio = request.form['txtAnio']
-        
-        curAct = mysql.connection.cursor()
-        curAct.execute('update albums1 set titulo=%s,artista=%s,anio=%s where id_album=%s',(titulo, artista, anio, id))
+        curAct=mysql.connection.cursor()
+        curAct.execute('update albums1 set titulo= %s,artista= %s,anio= %s where id_album= %s',(Vartitulo,Varartista,Varanio,id))
         mysql.connection.commit()
-        
-        flash('Album actualizado correctamente')
-        return redirect(url_for('index'))
     
-@app.route('/eliminar/<id>')
+    flash('Se actualizo el Album '+ Vartitulo)
+    return redirect(url_for('index'))
+
+@app.route('/borrar/<id>')
+def borrar(id):
+    cursorID=mysql.connection.cursor()
+    cursorID.execute('select * from albums1 where id_album=%s',(id))
+    albumELi=cursorID.fetchone()
+    return render_template('eliminaralbum.html',albumEli=albumELi)
+
+@app.route('/eliminar/<id>',methods=['POST'])
 def eliminar(id):
-    curEliminar=mysql.connection.cursor()
-    curEliminar.execute('select * from albums1 where id_album = %s ', (id,))
-    consulID1 = curEliminar.fetchone()
-    
-    return render_template('eliminaralbum.html', album=consulID1)
-
-@app.route('/delete/<id>', methods=['POST'])
-def delete(id):
-    if request.method == 'POST':
-        
-        titulo = request.form['txtTitulo']
-        artista = request.form['txtArtista']
-        anio = request.form['txtAnio']
-        
-        curAct = mysql.connection.cursor()
-        curAct.execute('delete from albums1 where id_album=%s',(id))
+    if request.method =='POST':
+        curEli=mysql.connection.cursor()
+        curEli.execute('delete from albums1 where id_album= %s',(id))
         mysql.connection.commit()
         
-        flash('Album actualizado correctamente')
-        return redirect(url_for('index'))
-    
+    flash('Se Elimino el Album ')
+    return redirect(url_for('index'))
 
-# Lineas que ejecutan el servidor
-if __name__ =='__main__':
-    app.run(port= 5000, debug=True)
+#ejecucion del Servidor en el Puerto 5000
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
